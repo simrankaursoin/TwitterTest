caff56c4cb1c09ab4e2c2f3c39ed68cce7f4bebc
Simran Soin <simransoin@107.0.0.10.in-addr.arpa.localnet>
2018-07-19 15:14:57 -0400

    Update twitter_streaming to sort out retweets and only display official tweets from pycon accounts. Also uses twitter scraper to achieve similar results.

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -1,6 +1,6 @@
 # twitter_streaming.py
 from secure import ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET
-from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+from twitter import Twitter, OAuth
 
 #connects to the twitter API
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
@@ -12,22 +12,14 @@ def collect_tweets(keywords):
     all_tweets = []    
     all_users = []  
     all_times = [] 
-    '''
-    finds 10 tweets containing anything from the list of keywords
-    '''
     tweets = twitter.search.tweets(q=" OR ".join(keywords),
-                                   result_type='recent', lang='en', count = 10)
+                                   result_type='recent', lang='en', count = 1000)
     tweet = tweets["statuses"]
-    '''
-    for each tweet containing a keyword:
-        add the text to all_tweets
-        add the username of the person who tweeted it to all_users
-        add the timestamp of the tweet to all_times
-    '''
     for i in tweet:
-        all_tweets.append(i["text"])
-        all_users.append(i["user"]["screen_name"])
-        all_times.append(i["created_at"])
+        if "pycon" in i["user"]["screen_name"] and i["text"][:2] != "RT":
+            all_tweets.append(i["text"])
+            all_users.append(i["user"]["screen_name"])
+            all_times.append(i["created_at"])
     '''
     Format document.
     Add all the tweets, usernames, and timestamps to document
@@ -41,8 +33,8 @@ def collect_tweets(keywords):
     document.write("\r\n \r\nTIMESTAMPS : \r\n")
     for i in all_times:
         document.write(i + "\r\n \r\n")
- 
+        
 #list of keywords to search through tweets
-keywords = ["#pycon2018", "#PyCon", "PyCon"]
+keywords = ["#pycon2018", "#PyCon", "pycon", "PyCon"]
 #call the function to search through tweets and create text file
 collect_tweets(keywords)
