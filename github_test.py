76d0a5333736f08fac77257a90ff8f9308478d6f
Simran Soin <simransoin@115.0.0.10.in-addr.arpa.localnet>
2018-07-20 12:11:26 -0400

    Add comments

--- github_test.py
+++ github_test.py
@@ -1,13 +1,17 @@
 import requests
 import json
+# username of github account being accessed
 username = "nedbat"
+
 user = requests.get('https://api.github.com/users/' + username)
 userItem = json.loads(user.text or user.content)
-print("username: ", userItem["login"])
 print(userItem["public_repos"], "public repos")
+
+#repoItem is an object that contains data about all created git repositories
 repos = requests.get(userItem["repos_url"])
 repoItem = json.loads(repos.text)
 print("List of python repo names:")
 for repo in repoItem:
+    # if the repo contains python, its name is printed
     if repo["language"] == "Python":
         print("\t" + repo["name"])
