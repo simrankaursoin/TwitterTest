2d36524db537fd25d642f76f42a3a0ce7b5d7c9a
Simran Soin <simransoin@115.0.0.10.in-addr.arpa.localnet>
2018-07-20 12:07:44 -0400

    Generalize method of accessing urls for efficiency

--- github_test.py
+++ github_test.py
@@ -4,7 +4,7 @@ user = requests.get('https://api.github.com/users/nedbat')
 userItem = json.loads(user.text or user.content)
 print("username: ", userItem["login"])
 print(userItem["public_repos"], "public repos")
-repos = requests.get('https://api.github.com/users/nedbat/repos')
+repos = requests.get(userItem["repos_url"])
 repoItem = json.loads(repos.text)
 print("List of python repo names:")
 for repo in repoItem:
