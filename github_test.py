502a1e38f12008037b38530d88c1dd7ea00ab64b
Simran Soin <simransoin@Simrans-MacBook-Pro.local>
2018-07-29 20:36:10 -0400

    Edit github_test.py using flake8

--- github_test.py
+++ github_test.py
@@ -5,31 +5,35 @@ import csv
 from secure2 import USER, PASS
 # secure2.py contains username and password for API auth
 
-#now is today's date and time
+# now is today's date and time
 now = datetime.datetime.now()
 
 # username of github account being accessed
 username = "nedbat"
-    
-#repoItem is an object that contains data about all created git repositories
-repos = requests.get("/".join(["https://api.github.com/users", username, "repos"]), auth=(USER, PASS))
+
+# repoItem is an object that contains data about all created git repositories
+repos = requests.get("/".join(["https://api.github.com/users",
+                     username, "repos"]), auth=(USER, PASS))
 repoItem = json.loads(repos.text)
 
-#make csv file
+# make csv file
 with open('repo_data.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer = csv.writer(csvfile, delimiter=',', quotechar='|',
+                        quoting=csv.QUOTE_MINIMAL)
     writer.writerow(["Name of Python Repo", "Last Commit Date"])
     for repo in repoItem:
         languages = requests.get(repo["languages_url"], auth=(USER, PASS))
         languageItem = json.loads(languages.text)
         if "Python" in languageItem:
-            commits = requests.get("/".join(["https://api.github.com/repos", username, repo["name"], "commits"]), auth=(USER, PASS))
+            commits = requests.get("/".join(["https://api.github.com/repos",
+                                   username, repo["name"], "commits"]),
+                                   auth=(USER, PASS))
             commitsItem = json.loads(commits.text)
             for item in commitsItem:
                 commit_month = item["commit"]["committer"]["date"][:7]
                 break
-            #compares most recent commit year/month to current year/month
+            # compares most recent commit year/month to current year/month
             current_month = str(now)[:7]
             if commit_month == current_month:
-                writer.writerow([repo["name"], item["commit"]["committer"]["date"]])
-                
+                writer.writerow([repo["name"],
+                                item["commit"]["committer"]["date"]])
